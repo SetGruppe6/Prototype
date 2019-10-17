55ff89dd870251f53e78bb30fbf92477776bda4a
Are <aregulliksen@gmail.com>
2019-10-17 12:06:31 +0200

    La til test for å teste lengden på arrangementsnavn

--- src/main/java/Arrangement.java
+++ src/main/java/Arrangement.java
@@ -1,3 +1,15 @@
 public class Arrangement {
-    public int tall = 1;
+
+    public static String isNameLongEnough (String name){
+
+        if (name.length() >= 5 && name.length() < 65){
+            return "name Ok";
+        } else if (name.length() >= 65){
+            return "name too long";
+        } else {
+            return "name too short";
+        }
+
+    }
+
 }
--- src/test/java/ArrangementTest.java
+++ src/test/java/ArrangementTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ArrangementTest {
+
+    @Test
+    public void was_Name_Long_Enough(){
+
+        assertEquals("name Ok", Arrangement.isNameLongEnough("Vasaloppet"));
+        assertEquals("name too long", Arrangement.isNameLongEnough("VasaloppetVasaloppetVasaloppetVasaloppetVasaloppetVasaloppetVasaloppetVasaloppetVasaloppetVasaloppetVasaloppetVasaloppetVasaloppet"));
+        assertEquals("name too short", Arrangement.isNameLongEnough("Vasa"));
+
+
+    }
 }
